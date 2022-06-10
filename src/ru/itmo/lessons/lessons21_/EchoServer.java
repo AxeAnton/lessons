5b44ef1aacfb4b70ec0a5fe6225ba612b877ecd2
PC <axespb@gmail.com>
2022-06-10 12:37:15 +0300

    1.4

---
+++ src/ru/itmo/lessons/lessons21_/EchoServer.java
@@ -0,0 +1,64 @@
+package ru.itmo.lessons.lessons21_;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+
+public class EchoServer {
+
+
+    private int port;
+    private Connection connection;
+    private int count;
+
+    public EchoServer(int port){
+        this.port = port;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void start() throws IOException, ClassNotFoundException {
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Server started...");
+            while (true){
+                Socket socket = serverSocket.accept();
+                connection = new Connection(socket);
+                count += 1;
+
+                SimpleMessage message = connection.readMessage();
+
+            }
+        }
+    }
+
+    private void printMessage(SimpleMessage message){
+        System.out.println("получено сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        int port = 8090;
+        EchoServer messageServer = new EchoServer(port);
+        try {
+            messageServer.start();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
